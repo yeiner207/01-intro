558d572fe12cd30136f9508a881f4e4138b6e790
Tu Nombre <tuemail@ejemplo.com>
2025-08-25 17:27:42 -0500

    segunda parte

--- biblioteca.js
+++ biblioteca.js
@@ -1,1 +1,27 @@
-function createbook(title,author)
+let books=[];
+function createbook(title,author,genre,isbn){
+    return {
+        id : Date.now(),
+        title : title,
+        author : author,
+        genre : genre,
+        isbn : isbn,
+        isAvailable : true,
+        borrowedBy: null,
+        borrowedAt: null,
+        dueDate: null,
+        createdAt : new Date().toDateString()
+    };
+}
+
+function addToLibrary(booksarray,tiitle,author,genre,isbn){
+    const newbook = createbook(tiitle,author,genre,isbn);
+    booksarray.push(newbook);
+}
+
+addToLibrary(books, "Cien Años de Soledad", "Gabriel García Márquez", "Realismo Mágico", "1234567890");
+
+console.log(books);
+addToLibrary(books, "pepito", "yeiner", "terror", "1089088383");
+
+console.log(books);
